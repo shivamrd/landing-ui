ffb9393d885b06bf5ecfa5dd7e827c6076e2eaf6
Vidit Pokhral <viditpokhral@gmail.com>
2026-02-12 17:58:22 +0530

    Added about Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AboutUs from './pages/About';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        
       </Routes>
     </Router>
   );
