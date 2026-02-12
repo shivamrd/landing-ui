e5fa0c1814cfaa3fc6ed156f28f2baf5dd8cf197
Vidit Pokhral <128287510+viditpokhral@users.noreply.github.com>
2026-02-12 07:31:07 +0000

    Resolved conflicts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,7 @@ import Hero from "../components/Home/hero/Hero";
 import Logos from "../components/Home/logos/Logos";
 import AutoFeatures from "../components/Home/autoFeature/AutoFeatures";
 import Features from "../components/Home/features/Features";
-import Hero from "../sections/Home/hero/Hero";
-import Logos from "../sections/Home/logos/Logos";
-import AutoFeatures from "../sections/Home/autoFeature/AutoFeatures";
-import FAQ from "../sections/Home/Faq/FAQ";
+import FAQ from "../components/Home/Faq/FAQ";
 
 
 
